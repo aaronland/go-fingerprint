c0bd9b599fe5d723b639a5f74b22ee99bcb643d2
thisisaaronland <devnull@localhost>
2023-07-05 18:14:31 -0700

    snapshot: start working through bezier curve changes

--- svg/document.go
+++ svg/document.go
@@ -4,8 +4,6 @@ import (
 	"fmt"
 	"image"
 	"math"
-	"strconv"
-	"strings"
 
 	"github.com/fogleman/gg"
 )
@@ -43,89 +41,14 @@ func (doc *Document) ToImage(max_dimension float64) (image.Image, error) {
 
 	dc := gg.NewContext(w, h)
 
-	for _, p := range doc.Paths {
+	for idx, p := range doc.Paths {
 
-		coords, err := p.Coordinates()
+		err := p.Draw(dc, scale)
 
 		if err != nil {
-			return nil, fmt.Errorf("Failed to derive coordinates, %w", err)
+			return nil, fmt.Errorf("Failed to draw path at offset %d, %w", idx, err)
 		}
-
-		count := len(coords)
-
-		dc.NewSubPath()
-
-		for i := 0; i < count; i++ {
-			x := coords[i][0] * scale
-			y := coords[i][1] * scale
-			dc.LineTo(x, y)
-		}
-
-		dc.ClosePath()
-
-		dc.SetHexColor(p.HexColor())
-		dc.Fill()
 	}
 
 	return dc.Image(), nil
-}
-
-// Path is a struct representing an individual path element in a fingerprint SVG drawing.
-type Path struct {
-	// Fill is the colour assigned to the path.
-	Fill string `xml:"fill,attr"`
-	// FillOpacity is the opacity of the colour (assigned to the path).
-	FillOpacity float64 `xml:"fill-opacity,attr"`
-	// D is the SVG-encoded value of the path.
-	D string `xml:"d,attr"`
-}
-
-// HexColor will return the 8-digit hexidecial color (fill + fill opacity) for the path
-func (p *Path) HexColor() string {
-
-	fill := p.Fill
-	opacity := p.FillOpacity
-
-	dec := int(opacity * 100.)
-
-	color := dec * 255 / 100
-	alpha := fmt.Sprintf("%02x", color)
-
-	return fill + alpha
-}
-
-// Coordinates will return a list of X and Y floating points for 'p' derived from its `D` property.
-func (p *Path) Coordinates() ([][2]float64, error) {
-
-	d := p.D
-	d = strings.TrimLeft(d, "M")
-	d = strings.TrimRight(d, "Z")
-
-	pairs := strings.Split(d, "L")
-	coords := make([][2]float64, len(pairs))
-
-	for i, pair := range pairs {
-
-		xy := strings.Split(pair, ",")
-
-		if len(xy) != 2 {
-			return nil, fmt.Errorf("Invalid coordinate (%s) at offset %d", pair, i)
-		}
-
-		x, err := strconv.ParseFloat(xy[0], 64)
-
-		if err != nil {
-			return nil, fmt.Errorf("Failed to parse x value '%s' at offset %d, %w", xy[0], i, err)
-		}
-
-		y, err := strconv.ParseFloat(xy[1], 64)
-
-		if err != nil {
-			return nil, fmt.Errorf("Failed to parse x value '%s' at offset %d, %w", xy[1], i, err)
-		}
-
-		coords[i] = [2]float64{x, y}
-	}
-
-	return coords, nil
 }
